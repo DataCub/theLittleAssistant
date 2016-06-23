c8a7cc177800ade69f76819f7284e10df12c18b7
YASH TEKRIWAL <yt2c@virginia.edu>
2016-06-23 20:24:36 +0200

    youtube URLS

--- testing_files/youtube_testing.R
+++ testing_files/youtube_testing.R
@@ -3,10 +3,12 @@ library(curl)
 library(jsonlite)
 library(httr)
 
-tmp <- fromJSON(paste0("https://www.googleapis.com/youtube/v3/videos?",
+tmp <- fromJSON(paste0("https://www.googleapis.com/youtube/v3/videos?", #everything after '?' is parameters being passed, '&' separates the argument
                        "part=snippet&chart=mostPopular&key=AIzaSyARX7-F4xQnLrSgUQi6MjAcpPcLtZwhkZY"))
-titles <- tmp$items$snippet$title
-etag <- tmp$items
-class(tmp)
-View(tmp)
+
+titles <- tmp$items$snippet$title # all the video titles 
+ids <- tmp$items$id
+
+urls <- as.character(sapply(ids, function(x) {paste0("https://www.youtube.com/watch?v=", x)})) # all the urls 
+
 
