5ce14a74aebc2e08c414ede7a5b7876990abbc16
PT <ptinsley1>
2016-06-23 16:58:23 +0200

    sports & twitter working

---
+++ twitter_sports/app.R
@@ -0,0 +1,57 @@
+library(shiny)
+library(httr)
+library(dplyr)
+library(magrittr)
+library(jsonlite)
+library(lubridate)
+library(shinythemes)
+
+source("carouselPanel.R")
+
+runApp(list(ui = fluidPage(
+  tags$head(tags$script('!function(d,s,id){var js,fjs=d.getElementsByTagName(s)    [0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");')),
+  titlePanel(""),
+  sidebarLayout(
+    sidebarPanel(
+      a("Sports", class="twitter-timeline",
+        href = "https://twitter.com/Complex_Sports")
+      ), 
+    mainPanel(
+      
+      carouselPanel(auto.advance=TRUE,
+                    dataTableOutput("top"),
+                    dataTableOutput("upcoming")
+      )
+    ) 
+  )
+)
+, server = function(input, output, session){
+  top <- GET("http://api.themoviedb.org/3/movie/now_playing?api_key=bc430e79d5377e1028b278f358f45b68")
+  upcoming <- GET("http://api.themoviedb.org/3/movie/upcoming?api_key=bc430e79d5377e1028b278f358f45b68")
+  
+  top.content <- content(top, as='text', encoding='UTF-8') %>% prettify %>% fromJSON
+  upcoming.content <- content(upcoming, as='text', encoding='UTF-8') %>% prettify %>% fromJSON
+  
+  top.movies <- top.content$results %>% na.omit %>% filter(original_language=='en') %>%
+    select(title,release_date,backdrop_path,popularity) %>%
+    transmute(`Hot Movies`=title, 
+              Released=release_date, 
+              Popularity=1:length(popularity))
+  
+  upcoming.movies <- upcoming.content$results %>% na.omit %>%
+    select(title,release_date,backdrop_path,popularity) %>%
+    transmute(`Upcoming Movies`=title, 
+              Coming=release_date, 
+              Popularity=1:length(popularity))
+  
+  output$top <- renderDataTable(top.movies[1:8,], options = list(
+    searching=FALSE,
+    info=FALSE,
+    paging=FALSE))
+  output$upcoming <- renderDataTable(upcoming.movies[1:8,], options = list(
+    searching=FALSE,
+    info=FALSE,
+    paging=FALSE))
+}
+)
+)
